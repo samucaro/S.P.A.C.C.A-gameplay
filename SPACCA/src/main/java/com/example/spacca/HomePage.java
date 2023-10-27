b6b3c060a942df0dd2bb31b1ac49085e4adb186e
Samuel Caro <samucaro60@gmail.com>
2023-10-27 14:26:55 +0200

    Rimozione cartella

--- SPACCA/src/main/java/com/example/spacca/HomePage.java
+++ SPACCA/src/main/java/com/example/spacca/HomePage.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class HomePage extends Application {
@@ -15,6 +17,14 @@ public class HomePage extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         try {
+            // Creazione di un oggetto ImageView
+            ImageView imageView = new ImageView();
+
+            // Caricamento dell'immagine
+            Image image = new Image("logo.jpg");
+
+            // Assegnazione dell'immagine all'ImageView
+            imageView.setImage(image);
             Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
