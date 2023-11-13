b05f44836a952ac4f615d7f1dc64df362836c480
Samuel Caro <samucaro60@gmail.com>
2023-11-13 17:51:13 +0100

    Commit marge unione developS e master

--- Gioco/src/main/java/com/example/gioco/AccessPlayerPageController.java
+++ Gioco/src/main/java/com/example/gioco/AccessPlayerPageController.java
@@ -1,11 +1,47 @@
 package com.example.gioco;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.ParallelCamera;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AccessPlayerPageController {
-
+    private Parent root;
+    private Scene scene;
     @FXML
     public void initialize() {
 
+    }
+
+    public void switchOvalPane(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Partitonza.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setMinWidth(900);
+        stage.setMinHeight(600);
+        scene = new Scene(root);
+        impostaListener(scene);
+        ParallelCamera cam = new ParallelCamera();
+        cam.setFarClip(2000);
+        cam.setNearClip(0.5);
+        scene.setCamera(cam);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void impostaListener(Scene scene){
+        scene.widthProperty().addListener((obs, oldVal, newVal) -> {
+            OvalPaneController.setScenaX((Double) newVal);
+            MainController.setScenaX((Double) newVal);
+        });
+
+        scene.heightProperty().addListener((obs, oldVal, newVal) -> {
+            OvalPaneController.setScenaY((Double) newVal);
+            MainController.setScenaY((Double) newVal);
+        });
     }
 }
--- Gioco/src/main/java/com/example/gioco/HomePageController.java
+++ Gioco/src/main/java/com/example/gioco/HomePageController.java
@@ -77,33 +77,6 @@ public class HomePageController {
         stage.show();
     }
 
-    /*
-    public void switchOvalPane(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("Partitonza.fxml"));
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setMinWidth(900);
-        stage.setMinHeight(600);
-        scene = new Scene(root);
-        impostaListener(scene);
-        ParallelCamera cam = new ParallelCamera();
-        cam.setFarClip(2000);
-        cam.setNearClip(0.5);
-        scene.setCamera(cam);
-        stage.setScene(scene);
-        stage.show();
-    }
-    public void impostaListener(Scene scene){
-        scene.widthProperty().addListener((obs, oldVal, newVal) -> {
-            OvalPaneController.setScenaX((Double) newVal);
-            MainController.setScenaX((Double) newVal);
-        });
-
-        scene.heightProperty().addListener((obs, oldVal, newVal) -> {
-            OvalPaneController.setScenaY((Double) newVal);
-            MainController.setScenaY((Double) newVal);
-        });
-    }
-     */
     private void setPassword(String psw) {
         this.psw = psw;
     }
